cca961c4422696e98dac301184334312692bb249
Maneesha Walisundera <maneesha@opensource.lk>
2025-06-27 10:19:15 +0530

    chore: add pwgen.

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 sudo apt update && sudo apt upgrade
 
-sudo apt install git ufw
+sudo apt install git ufw pwgen
 
 #lynis check for hardening
 #cd ~ && sudo rm -rf ~/lynis && mkdir lynis && git clone https://github.com/CISOfy/lynis --depth=1 && sudo chown -R 0:0 lynis && cd lynis && sudo ./lynis audit system --pentest && cd ~ && sudo rm -rf ~/lynis
