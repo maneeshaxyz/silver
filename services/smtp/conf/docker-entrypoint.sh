52529024abe63eb22ebf81c141c01126af0cb70d
maneeshaxyz <maneesha@opensource.lk>
2025-07-14 17:12:45 +0530

    Chore/combined containers (#21)
    * docs: Update README.md
    
    * chore: added access to certs for keys.
    
    * chore: init dovecot auth setup.
    
    * chore: dovecot config setup.
    
    * chore: housekeeping and dovecot service.
    
     * fixed tls naming in smtp and dovecot folders.
    * Added dovecot service to compose.
    
    * chore: cleanup.
    
    * Removed mail volume to compose.
    * added mail network andmail volume
    
    * chore: cleanup.
    
    * chore: remove postfix conf.
    
    * fix: added initial dovecot sasl settings and fixed hostname issue.
    
    * chore: cleanup for testing.
    
    * created volume for cert.
    * temporary generic container for tests and running test suite
    * simplifying
    
    * chore: save state/
    
    * chore: add staging.
    
    * chore: add healthcheck.
    
    * chore: healthcheck fix.
    
    * chore: remove autoreload.

--- services/smtp/conf/docker-entrypoint.sh
+++ services/smtp/conf/docker-entrypoint.sh
@@ -5,16 +5,18 @@ set -e
 echo "INFO: Initializing Postfix configuration..."
 
 # postfix config 
-postconf -e "myhostname=${MYHOSTNAME}"
-postconf -e "mydestination=${MYDESTINATION}"
-postconf -e "inet_interfaces=${INET_INTERFACES}"
+postconf -e "myhostname = ${MYHOSTNAME}"
+postconf -e "mydestination = ${MYDESTINATION}"
+postconf -e "inet_interfaces = ${INET_INTERFACES}"
+postconf -e "smtpd_tls_cert_file = /etc/postfix/tls/fullchain.pem"
+postconf -e "smtpd_tls_key_file = /etc/postfix/tls/privkey.pem"
+postconf -e "smtpd_sasl_type = dovecot"
+postconf -e "smtpd_sasl_path = private/auth"
+postconf -e "smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
 
 echo "INFO: Running 'postfix check'..."
 
-if ! postfix check; then
-    echo "ERROR: 'postfix check' found errors. Exiting." >&2
-    exit 1
-fi
+postfix check
 
 echo "INFO: 'postfix check' completed successfully."
 
