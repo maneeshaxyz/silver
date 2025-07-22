8d6a2e7bf44469c19fd657c5ce5105d0852974e5
maneeshaxyz <maneesha@opensource.lk>
2025-07-22 13:24:56 +0530

    Chore/env change (#22)
    * chore: env change test.
    
    * chore: add TLS logging and test default log
    
    * chore: persist postfix volume.
    
    * fix: fix volume bug.
    
    * chore: add master.cf with submission.

--- services/smtp/conf/docker-entrypoint.sh
+++ services/smtp/conf/docker-entrypoint.sh
@@ -8,14 +8,17 @@ echo "INFO: Initializing Postfix configuration..."
 postconf -e "myhostname = ${MYHOSTNAME}"
 postconf -e "mydestination = ${MYDESTINATION}"
 postconf -e "inet_interfaces = ${INET_INTERFACES}"
-postconf -e "smtpd_tls_cert_file = /etc/postfix/tls/fullchain.pem"
-postconf -e "smtpd_tls_key_file = /etc/postfix/tls/privkey.pem"
+# TLS
+postconf -e "smtpd_tls_security_level = may"
+postconf -e "smtp_tls_loglevel = 1"
+postconf -e "smtpd_tls_cert_file = /le-ssl/letsencrypt/live/maneesha.dev/fullchain.pem"
+postconf -e "smtpd_tls_key_file = /le-ssl/letsencrypt/live/maneesha.dev/privkey.pem"
+# SASL
 postconf -e "smtpd_sasl_type = dovecot"
 postconf -e "smtpd_sasl_path = private/auth"
 postconf -e "smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
 
 echo "INFO: Running 'postfix check'..."
-
 postfix check
 
 echo "INFO: 'postfix check' completed successfully."
