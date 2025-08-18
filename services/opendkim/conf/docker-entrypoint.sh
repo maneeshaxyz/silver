e25d9dc5a9afe0e9ed86208c820e36d2be3a6aef
Maneesha Walisundera <maneesha@opensource.lk>
2025-08-18 14:25:55 +0530

    chore: fix.

--- services/opendkim/conf/docker-entrypoint.sh
+++ services/opendkim/conf/docker-entrypoint.sh
@@ -2,10 +2,6 @@
 DKIM_KEY_DIR="/etc/opendkim/keys"
 DKIM_SELECTOR="mail"
 
-sed -i "s/#Domain.*/Domain $MYHOSTNAME/" /etc/opendkim.conf
-sed -i "s/#Selector.*/Selector $DKIM_SELECTOR/" /etc/opendkim.conf
-sed -i "s|#KeyFile.*|KeyFile $DKIM_KEY_DIR/$MYHOSTNAME/$DKIM_SELECTOR.private|" /etc/opendkim.conf
-
 # Generate DKIM keys if they don't exist
 if [ ! -f "$DKIM_KEY_DIR/$MYHOSTNAME/$DKIM_SELECTOR.private" ]; then
     echo "Generating DKIM keys for $MYHOSTNAME..."
@@ -13,6 +9,12 @@ if [ ! -f "$DKIM_KEY_DIR/$MYHOSTNAME/$DKIM_SELECTOR.private" ]; then
     opendkim-genkey -t -s $DKIM_SELECTOR -d $MYHOSTNAME -D "$DKIM_KEY_DIR/$MYHOSTNAME"
     chown opendkim:opendkim "$DKIM_KEY_DIR/$MYHOSTNAME"/*
     chmod 600 "$DKIM_KEY_DIR/$MYHOSTNAME/$DKIM_SELECTOR.private"
+
+    sleep 2
+
+    sed -i "s/#Domain.*/Domain $MYHOSTNAME/" /etc/opendkim.conf
+    sed -i "s/#Selector.*/Selector $DKIM_SELECTOR/" /etc/opendkim.conf
+    sed -i "s|#KeyFile.*|KeyFile $DKIM_KEY_DIR/$MYHOSTNAME/$DKIM_SELECTOR.private|" /etc/opendkim.conf
 fi
 
 exec opendkim -f
