650bfd2449ab1b45123d95b94f1eaf5cf8840b9b
Maneesha Walisundera <maneesha@opensource.lk>
2025-08-20 10:29:21 +0530

    chore: permissions fix.

--- services/opendkim/conf/docker-entrypoint.sh
+++ services/opendkim/conf/docker-entrypoint.sh
@@ -3,4 +3,7 @@
 echo "Starting key generation for ${MYHOSTNAME}"
 opendkim-genkey -D /etc/dkimkeys -d ${MYHOSTNAME} -s 2025
 
+chown opendkim:opendkim /etc/dkimkeys/2025.private
+chmod 600 /etc/dkimkeys/2025.private
+
 exec opendkim -f
