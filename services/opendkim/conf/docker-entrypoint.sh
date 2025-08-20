b0113f06d91c6e1cc27094df0fbc200a8f1df978
Maneesha Walisundera <maneesha@opensource.lk>
2025-08-20 13:01:12 +0530

    chore: fix.

--- services/opendkim/conf/docker-entrypoint.sh
+++ services/opendkim/conf/docker-entrypoint.sh
@@ -1,9 +1,33 @@
 #! /bin/bash
 
-echo "Starting key generation for ${MYHOSTNAME}"
-opendkim-genkey -D /etc/dkimkeys -d ${MYHOSTNAME} -s 2025
+echo "Starting key generation for ${DKIM_DOMAIN}"
+opendkim-genkey -D /etc/dkimkeys -d ${DKIM_DOMAIN} -s ${DKIM_SELECTOR}
 
-chown opendkim:opendkim /etc/dkimkeys/2025.private
-chmod 600 /etc/dkimkeys/2025.private
+# Create KeyTable
+cat > /etc/opendkim/KeyTable << EOF
+${DKIM_SELECTOR}._domainkey.${DKIM_DOMAIN} ${DKIM_DOMAIN}:${DKIM_SELECTOR}:/etc/dkimkeys/${DKIM_SELECTOR}.private
+EOF
+
+# Create SigningTable  
+cat > /etc/opendkim/SigningTable << EOF
+*@${DKIM_DOMAIN} ${DKIM_SELECTOR}._domainkey.${DKIM_DOMAIN}
+EOF
+
+# Create TrustedHosts
+cat > /etc/opendkim/TrustedHosts << EOF
+127.0.0.1
+localhost
+0.0.0.0/0
+EOF
+
+chown opendkim:opendkim /etc/dkimkeys/${DKIM_SELECTOR}.private && \
+chown opendkim:opendkim /etc/opendkim/KeyTable && \
+chown opendkim:opendkim /etc/opendkim/SigningTable && \
+chown opendkim:opendkim /etc/opendkim/TrustedHosts
+
+chmod 600 /etc/dkimkeys/${DKIM_SELECTOR}.private && \
+chmod 644 /etc/opendkim/KeyTable && \
+chmod 644 /etc/opendkim/SigningTable && \
+chmod 644 /etc/opendkim/TrustedHosts
 
 exec opendkim -f
