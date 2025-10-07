9a91d81bbc5419677deb559a83020bf19ba2841e
maneesha <maneesha@opensource.lk>
2025-10-07 11:41:20 +0530

    chore: typo

--- services/config-scripts/gen-configs.sh
+++ services/config-scripts/gen-configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash ./config-scripts/gen-certbot-certs-conf.sh
+bash ./config-scripts/gen-certbot-certs.sh
 bash ./config-scripts/gen-opendkim-conf.sh
 bash ./config-scripts/gen-rspamd-conf.sh
 bash ./config-scripts/gen-postfix-conf.sh
